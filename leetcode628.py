e75a4c5561dcfb9fc182eaa49879a2a77715abeb
Marco2018 <513045631@qq.com>
2018-08-12 13:23:45 +0800

    upload

---
+++ leetcode628.py
@@ -0,0 +1,1 @@
+class Solution(object):    def maximumProduct(self, nums):        """        :type nums: List[int]        :rtype: int        """        nums.sort()        return max(nums[-2]*nums[-3],nums[0]*nums[1])*nums[-1]s = Solution()nums = [-5,-6,1,2,3,4]k = 6print(s.maximumProduct(nums))
