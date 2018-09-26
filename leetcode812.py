146b33f7885494e6a217b4cee2e0a1625ba89515
Marco2018 <513045631@qq.com>
2018-09-26 15:24:09 +0800

    9-26

---
+++ leetcode812.py
@@ -0,0 +1,1 @@
+class Solution:    def largestTriangleArea(self, p):        return max(0.5 * abs(i[0] * j[1] + j[0] * k[1] + k[0] * i[1] - j[0] * i[1] - k[0] * j[1] - i[0] * k[1])                   for i, j, k in itertools.combinations(p, 3))solution:https://leetcode.com/problems/largest-triangle-area/discuss/122711/C++JavaPython-Solution-with-Explanation-and-Prove
