d0156eb33985e653a6f35eeddec159c862ec08a0
Marco2018 <513045631@qq.com>
2018-10-04 10:53:04 +0800

    10-4

---
+++ string/leetcode3.py
@@ -0,0 +1,15 @@
+def lengthOfLongestSubstring(s):
+    start = maxLength = 0
+    usedChar = {}
+    for i in range(len(s)):
+        if s[i] in usedChar and start <= usedChar[s[i]]:
+            start = usedChar[s[i]] + 1
+        else:
+            maxLength = max(maxLength, i - start + 1)
+        usedChar[s[i]] = i
+    return maxLength
+s="abcabcbb"
+print(lengthOfLongestSubstring(s))
+
+
+Given a string, find the length of the longest substring without repeating characters.
